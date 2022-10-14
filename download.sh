07336af1c40beac146db5a9c47ddc6bf0f868640
justapig9020 <justapig9020@gmail.com>
2022-10-14 23:28:45 +0800

    Add download and evaluation script

---
+++ download.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+wget https://www.dropbox.com/s/luh91s3qzwlws42/intent_best.pt
+wget https://www.dropbox.com/s/q5bbx9q69d1iajy/slot_best.pt
--- slot_tag.sh
+++ slot_tag.sh
@@ -1,4 +1,3 @@
 # "${1}" is the first argument passed to the script
 # "${2}" is the second argument passed to the script
-python3 train_slot.py
-python3 test_slot.py --test_file "$1" --ckpt_path ckpt/slot/best.pt --perd_file "$2"
+python3 test_slot.py --test_file "$1" --ckpt_path ./slot_best.pt --perd_file "$2"
